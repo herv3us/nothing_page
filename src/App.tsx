9986f9265ad3163d1695aed0dea3e3c92d6b3453
Sofia Hervéus <sofia@herveus.se>
2024-11-02 08:15:31 +0100

    add tailwind and start on translations

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import image from './assets/images/gamer43.png';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="p-0 m-0 flex flex-col items-center justify-center bg-gradient-to-b from-indigo-500 to-zinc-200 h-screen w-screen">
+      <p className="text-lg">There is nothing to see here.</p>
+      <img src={image} alt="gamer43" className="w-[400px] sm:w-[850px]" />
+      <p className="text-lg">This page is empty.</p>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/assets/translations.js
@@ -0,0 +1,12 @@
+export const texts = {
+  en: {
+    title: 'Hello, world!',
+    subTitle: 'There is nothing to see here.',
+    description: 'This page is empty at the moment.',
+  },
+  sv: {
+    title: 'Hej, världen!',
+    subTitle: 'Det finns inget att se här.',
+    description: 'Denna sidan är tom för tillfället.',
+  },
+};
